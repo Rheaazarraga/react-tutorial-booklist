1ba5cd2206a3dd91269e6ea41055c1e9b377ce38
Rhea <rheaazarraga@gmail.com>
2022-04-05 21:58:08 -0700

    add jsx rule comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import ReactDom from 'react-dom';
 // stateless functional component
 // always return JSX
 
+/* JSX rules: 
+return single element
+div / section / article or Fragment
+use camelCase for html attribute
+className instead of class
+close every element
+formatting
+*/
+
+
 function Greeting() {
 
   return(
