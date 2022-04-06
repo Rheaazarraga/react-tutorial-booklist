befd9fd47c1c006937cf8db844b1fcddde89d1f2
Rhea <rheaazarraga@gmail.com>
2022-04-05 22:21:33 -0700

    create nested components within book component to render multiple books

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// stateless functional component
-// always return JSX
-
-/* JSX rules: 
-return single element
-div / section / article or Fragment
-use camelCase for html attribute
-className instead of class
-close every element
-formatting
-*/
-
-// nested components, react tools
 
 function BookList() {
 
@@ -21,17 +8,24 @@ function BookList() {
     <section>
       <Book />
       <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
 const Book = () => {
-  return <article>
-    this is a book
+  return (<article>
+    <Image/>
+    <Title />
+    <Author />
   </article>
+  );
 };
+
+const Image = () => (<img src='https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg' alt='' />
+);
+
+const Title = () => <h1>Half Baked Harvest</h1>;
+const Author = () => <h1>Tieghan Gerard</h1>;
+
 
 ReactDom.render(<BookList/>, document.getElementById('root'));
