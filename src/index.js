214613c18470bfcda0b31213257f9b55f2c4a072
Rhea <rheaazarraga@gmail.com>
2022-04-06 22:19:52 -0700

    make booklist more dynamic by using objects to store book vars enable to pass props into Book copmonent

--- src/index.js
+++ src/index.js
@@ -2,35 +2,53 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+// set up vars
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg',
+  author: 'Tieghan Gerard',
+  title: 'Half Baked Harvest'
+};
 
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/512SbYYGaWL._SX402_BO1,204,203,200_.jpg',
+  author: 'Joshua Weissman',
+  title: 'Joshua Weissman: An Unapologetic Cookbook'
+};
+
+const thirdBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51hS7eLW65L._SX444_BO1,204,203,200_.jpg',
+  author: 'Andrew Rea',
+  title: 'Binging With Babish: 100 Recipes Recreated from Your Favorite Movies and TV Shows' 
+}
 function BookList() {
 
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img={firstBook.img}
+      title={firstBook.title}
+      author={firstBook.author}/>
+      <Book img={secondBook.img}
+      title={secondBook.title}
+      author={secondBook.author}/>
+      <Book img={thirdBook.img}
+      title={thirdBook.title}
+      author={thirdBook.author}/>
+
     </section>
   );
 }
 
-const title = 'Half Baked Harvest';
-const author = 'Tieghan Gerard';
-const img = 'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg';
+
+
 
 const Book = (props) => {
-  console.log('props---', props);
 
 
   return (
     <article className='book'>
-      <img src={img} alt='' />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      <p>{title}</p>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
