63dd2f09b1028887dd0ad04d02133ce8f53276d0
Rhea <rheaazarraga@gmail.com>
2022-04-06 21:51:21 -0700

    make remaining vars dynamic, start to introduce props

--- src/index.js
+++ src/index.js
@@ -17,25 +17,23 @@ function BookList() {
   );
 }
 
-const Book = () => {
+const title = 'Half Baked Harvest';
+const author = 'Tieghan Gerard';
+const img = 'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg';
 
-  const title = 'Half Baked Harvest';
-  const author = 'Tieghan Gerard';
+const Book = (props) => {
+  console.log('props---', props);
+
 
   return (
     <article className='book'>
-      <img src='https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg' alt='' />
+      <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
+      <p>{title}</p>
     </article>
   );
 };
-
-//const Image = () => ();
-
-// const Title = () => ;
-
-//const Author = () => ;
 
 
 ReactDom.render(<BookList />, document.getElementById('root'));
