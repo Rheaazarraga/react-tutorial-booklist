3b75a6484e59a15d52ab904374ba3948557d6bff
Rhea <rheaazarraga@gmail.com>
2022-04-07 21:18:40 -0700

    tutorial section on event handlers - create a button with an alert that listens for onclick

--- src/index.js
+++ src/index.js
@@ -61,14 +61,20 @@ function BookList() {
 }
 
 
-// passing detructured props
+// passing destructured props
 const Book = ({img, title, author}) => {
-  
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert('You chose this book');
+  }
+
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>Look inside</button>
     </article>
   );
 };
