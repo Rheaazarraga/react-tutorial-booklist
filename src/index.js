e745d3cd9abcc04ae6601f09e09f4cc450844db5
Rhea <rheaazarraga@gmail.com>
2022-04-07 20:52:20 -0700

    refactor separate book objs to an array and iterate over each book oject in the array to return the respective book. Pass modified props

--- src/index.js
+++ src/index.js
@@ -2,68 +2,52 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-// set up vars
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg',
-  author: 'Tieghan Gerard',
-  title: 'Half Baked Harvest'
-};
 
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/512SbYYGaWL._SX402_BO1,204,203,200_.jpg',
-  author: 'Joshua Weissman',
-  title: 'Joshua Weissman: An Unapologetic Cookbook'
-};
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg',
+    author: 'Tieghan Gerard',
+    title: 'Half Baked Harvest'
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/512SbYYGaWL._SX402_BO1,204,203,200_.jpg',
+    author: 'Joshua Weissman',
+    title: 'Joshua Weissman: An Unapologetic Cookbook'
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51hS7eLW65L._SX444_BO1,204,203,200_.jpg',
+    author: 'Andrew Rea',
+    title: 'Binging With Babish: 100 Recipes Recreated from Your Favorite Movies and TV Shows'
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51eU8l7A9oL._SX400_BO1,204,203,200_.jpg',
+    author: 'Matty Matheson',
+    title: 'Home Style Cookery: A Home Cookbook'
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51jEmzNkNVL._SX398_BO1,204,203,200_.jpg',
+    author: 'Samin Nosrat',
+    title: ' Salt, Fat, Acid, Heat: Mastering the Elements of Good Cooking'
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51fFkBSa5UL._SX402_BO1,204,203,200_.jpg',
+    author: 'Stanley Tucci',
+    title: ' The Tucci Table: Cooking With Family and Friends'
+  },
+];
 
-const thirdBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51hS7eLW65L._SX444_BO1,204,203,200_.jpg',
-  author: 'Andrew Rea',
-  title: 'Binging With Babish: 100 Recipes Recreated from Your Favorite Movies and TV Shows' 
-}
-
-const fourthBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51eU8l7A9oL._SX400_BO1,204,203,200_.jpg',
-  author: 'Matty Matheson',
-  title: 'Home Style Cookery: A Home Cookbook' 
-}
-
-const fifthBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51jEmzNkNVL._SX398_BO1,204,203,200_.jpg',
-  author: 'Samin Nosrat',
-  title: ' Salt, Fat, Acid, Heat: Mastering the Elements of Good Cooking' 
-}
-
-const sixthBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51fFkBSa5UL._SX402_BO1,204,203,200_.jpg',
-  author: 'Stanley Tucci',
-  title: ' The Tucci Table: Cooking With Family and Friends' 
-}
 
 function BookList() {
 
   return (
     <section className='booklist'>
-      <Book img={firstBook.img}
-      title={firstBook.title}
-      author={firstBook.author}
-      />
-      <Book img={secondBook.img}
-      title={secondBook.title}
-      author={secondBook.author}
-      />
-      <Book img={thirdBook.img}
-      title={thirdBook.title}
-      author={thirdBook.author}
-      />
-      <Book img={fourthBook.img}
-      title={fourthBook.title}
-      author={fourthBook.author}/>
-      <Book img={fifthBook.img}
-      title={fifthBook.title}
-      author={fifthBook.author}/>
-      <Book img={sixthBook.img}
-      title={sixthBook.title}
-      author={sixthBook.author}/>
+      {books.map((book)=> {
+        const {img, title, author} = book;
+        console.log(book);
+        return (
+         <Book book={book}></Book>
+        );
+      })}
 
     </section>
   );
@@ -71,9 +55,10 @@ function BookList() {
 
 
 
-// another way to pass props:
+// another way to pass props - destructured
 const Book = (props) => {
-  const { img, title, author } = props;
+  console.log(props);
+  const { img, title, author } = props.book;
 
   return (
     <article className='book'>
