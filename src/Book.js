f3185ed7ededdaf6e35a5c7182fdc1a172ba615d
Rhea <rheaazarraga@gmail.com>
2022-04-07 22:03:48 -0700

    move Book component into new file, import component back to index

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const Book = () => {
+// passing destructured props
+const Book = ({img, title, author}) => {
+
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  
+  //reference function:
+  const clickHandler = (e) => {
+    alert('You chose this book');
+  }
+
+  const authorInfo = (author) => {
+    alert('Please google for the author for other titles by them')
+    console.log(author);
+  };
+
   return (
-    <div>Book</div>
-  )
-}
+    <article className='book' onMouseOver={() => {
+      console.log(title);
+    }}>
+      <img src={img} alt='' />
+      <h1 onClick={()=> console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>Look inside</button>
+      <button type='button' onClick={() => authorInfo(author)}>More by this author</button>
+    </article>
+  );
+};
+
 
 export default Book
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {books} from './books';
+import Book from './Book';
 import './index.css';
 
 
@@ -21,34 +22,5 @@ function BookList() {
     </section>
   );
 }
-
-// passing destructured props
-const Book = ({img, title, author}) => {
-
-  // attribute, eventHandler
-  // onClick, onMouseOver
-  
-  //reference function:
-  const clickHandler = (e) => {
-    alert('You chose this book');
-  }
-
-  const authorInfo = (author) => {
-    alert('Please google for the author for other titles by them')
-    console.log(author);
-  };
-
-  return (
-    <article className='book' onMouseOver={() => {
-      console.log(title);
-    }}>
-      <img src={img} alt='' />
-      <h1 onClick={()=> console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type='button' onClick={clickHandler}>Look inside</button>
-      <button type='button' onClick={() => authorInfo(author)}>More by this author</button>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById('root'));
