2ad57c4ade11ee70a6b81701a1530a7a5bdb4b22
Rhea <rheaazarraga@gmail.com>
2022-04-07 21:00:38 -0700

    add id property to fix browser console warning about 'each child in a list should have a unique key prop'

--- src/index.js
+++ src/index.js
@@ -5,35 +5,41 @@ import './index.css';
 
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg',
     author: 'Tieghan Gerard',
     title: 'Half Baked Harvest'
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/512SbYYGaWL._SX402_BO1,204,203,200_.jpg',
     author: 'Joshua Weissman',
     title: 'Joshua Weissman: An Unapologetic Cookbook'
   },
   {
+    id: 3,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51hS7eLW65L._SX444_BO1,204,203,200_.jpg',
     author: 'Andrew Rea',
     title: 'Binging With Babish: 100 Recipes Recreated from Your Favorite Movies and TV Shows'
   },
   {
+    id: 4,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51eU8l7A9oL._SX400_BO1,204,203,200_.jpg',
     author: 'Matty Matheson',
     title: 'Home Style Cookery: A Home Cookbook'
   },
   {
+    id: 5,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51jEmzNkNVL._SX398_BO1,204,203,200_.jpg',
     author: 'Samin Nosrat',
     title: ' Salt, Fat, Acid, Heat: Mastering the Elements of Good Cooking'
   },
   {
+    id:6,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51fFkBSa5UL._SX402_BO1,204,203,200_.jpg',
     author: 'Stanley Tucci',
     title: ' The Tucci Table: Cooking With Family and Friends'
-  },
+  }
 ];
 
 
@@ -45,7 +51,8 @@ function BookList() {
       book param always points back to an item/single obj in the array */}
       {books.map((book)=> {
         return (
-         <Book book={book}></Book>
+         <Book key={book.id} 
+         book={book}></Book>
         );
       })}
 
