a2b15c38dd1a6d4dc920faf3e56da94f2baa1b49
Rhea <rheaazarraga@gmail.com>
2022-04-07 21:31:21 -0700

    implement more complex example of inline clickHandler for author

--- src/index.js
+++ src/index.js
@@ -72,12 +72,17 @@ const Book = ({img, title, author}) => {
     alert('You chose this book');
   }
 
+  const authorInfo = (author) => {
+    console.log(author);
+  };
+
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1 onClick={()=> console.log(title)}>{title}</h1>
       <h4>{author}</h4>
       <button type='button' onClick={clickHandler}>Look inside</button>
+      <button type='button' onClick={() => authorInfo(author)}>More by this author</button>
     </article>
   );
 };
